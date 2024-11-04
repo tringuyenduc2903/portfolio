3a2652705a8c50a5c9759b5e907c6289dd3c9ed5
Nguyễn Đức Trí <nguyenductri2k3@gmail.com>
2024-11-04 23:32:09 +0700

    Change Index page

---
+++ src/app/components/contact.js
@@ -0,0 +1,48 @@
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Contact() {
+  return (
+    <div className="h-max xl:px-12 lg:h-[22rem] p-6 sm:p-8 md:px-10 pt-32 md:pt-40 lg:pt-10 bg-gradient-to-tr dark:from-[#df51ad3e] dark:to-[#eb587d2a] from-[#f6ebeb90] to-[#fff8ec60] flex flex-col lg:flex-row mx-auto rounded-3xl shadow-md drop-shadow-2xl backdrop-blur-xl items-center justify-center lg:justify-between">
+      <div className="order-2 sm:w-[75%] lg:w-[80%] lg:basis-[60%] flex flex-col space-y-6 cirGradient bg-clip-text">
+        <h2 className="tracking-normal bg-gradient-to-br from-blue-400 via-indigo-600 to-red-500 bg-clip-text text-transparent text-2xl md:text-3xl lg:text-4xl font-semibold selection:text-gray-700 dark:selection:text-white/90">
+          Liên lạc
+        </h2>
+        <p className="text-sm md:text-md font-normal lg:text-lg xl:text-lg">
+          Nếu bạn có bất kỳ thắc mắc nào hoặc muốn thảo luận về các cơ hội tiềm
+          năng, vui lòng liên hệ với tôi bằng các liên kết sau:
+        </p>
+        <div className="grid grid-cols-2 text-[0.7rem] md:text-[0.85rem] lg:text-base gap-2 md:gap-3 lg:gap-4">
+          <Link
+            href="tel:0982213854"
+            className="flex align-middle items-center font-jak font-bold space-x-1 w-max"
+          >
+            <FaPhoneAlt className="text-[#518FC7] dark:text-[#2498fe] h-3 w-3 md:h-4 md:w-4" />
+            <h5 className="text-transparent dark:text-[#2498fe]">Gọi điện</h5>
+          </Link>
+          <Link
+            href="mailto:nguyenductri2k3@gmail.com"
+            className="flex align-middle items-center font-jak font-bold space-x-1 w-max"
+          >
+            <IoMdMail className="text-[#CA6298] dark:text-[#ff339c] h-4 w-4 md:h-5 md:w-5" />
+            <h5 className="text-transparent dark:text-[#ff339c]">Gửi thư</h5>
+          </Link>
+        </div>
+      </div>
+      <div className="order-1 lg:order-2 absolute -rotate-[15deg] lg:rotate-0 lg:flex top-6 right-4 md:top-10 md:right-12 lg:basis-[40%]">
+        <Image
+          src="Rectangle.svg"
+          className="select-none backdrop-shadow-xl h-24 w-24 md:h-40 md:w-44 lg:w-[15rem] lg:h-[16rem] xl:w-[16rem] xl:h-[18rem]"
+          alt="boo"
+          loading="lazy"
+          priority={false}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          width={750}
+          height={650}
+        />
+      </div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,73 @@
+"use client";
+
 import Image from "next/image";
+import Link from "next/link";
+import { useTheme } from "next-themes";
+import { BsArrowUpRight } from "react-icons/bs";
+import Experience from "@/app/components/experience";
+import Contact from "@/app/components/contact";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const { systemTheme, theme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <div className="lg:min-h-screen px-10 sm:px-20 md:px-32 lg:mb-12 lg:px-60 mx-auto max-w-[75rem]">
+      <div className="lg:h-screen 2xl:h-max max-w-[90rem] mx-auto">
+        <div className="flex flex-row justify-start items-center mt-20">
+          <Image
+            src={
+              currentTheme === "dark" ? "Avatar-black.svg" : "Avatar-white.svg"
+            }
+            className="select-none"
+            alt="avatar"
+            loading="lazy"
+            priority={false}
+            width={80}
+            height={80}
+          />
+          <div className="flex flex-col ml-4">
+            <h2 className="flex sm:text-2xl md:text-2xl lg:text-2xl">
+              <span className="font-semibold">Nguyễn Đức Trí</span>
+              <span>
+                <Image
+                  src={
+                    currentTheme === "dark" ? "boo-black.svg" : "boo-white.svg"
+                  }
+                  className="select-none"
+                  alt="boo"
+                  loading="lazy"
+                  priority={false}
+                  width={30}
+                  height={30}
+                />
+              </span>
+            </h2>
+            <h3>
+              <Link
+                href="https://www.linkedin.com/in/tringuyenduc2903"
+                target="_blank"
+                className="text-[#717171bb] flex items-center"
+              >
+                <p>@tringuyenduc2903</p>
+                <BsArrowUpRight className="stroke-1 h-3" />
+              </Link>
+            </h3>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="mt-12 text-base md:text-xl lg:text-xl xl:text-xl 2xl:text-xl">
+          <p className="sm:leading-6 md:leading-6 lg:leading-8">
+            Là lập trình viên có kinh nghiệm làm việc trong các dự án vừa và
+            nhỏ, tôi sẵn sàng tham gia phát triển framework Magento và Laravel.
+          </p>
+        </div>
+      </div>
+      <div className="mt-28 mx-auto">
+        <Experience />
+      </div>
+      <div className="mt-28 mx-auto">
+        <Contact />
+      </div>
     </div>
   );
 }
